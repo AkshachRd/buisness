a45f64cdda495000e951a9d736c20c8c1bf767d1
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2024-12-02 13:44:53 +0300

    Убрал лишнее. Поменял иконку.

--- components/CTASection.tsx
+++ components/CTASection.tsx
@@ -6,7 +6,7 @@ type CTASectionProps = {
 
 export default function CTASection({ onModalOpen }: CTASectionProps) {
   return (
-    <section className="w-full py-20 bg-blue-600 w-min-full">
+    <section className=" w-full py-20 bg-blue-600 w-min-full">
       <div className="container mx-auto px-6 text-center">
         {/* Заголовок с призывом */}
         <h2 className="text-3xl md:text-4xl font-bold text-white mb-6">
@@ -14,7 +14,7 @@ export default function CTASection({ onModalOpen }: CTASectionProps) {
         </h2>
         {/* Кнопка "Попробовать бесплатно" */}
         <Button
-          color="default"
+          className="light text-foreground bg-background "
           radius="full"
           variant="shadow"
           onClick={onModalOpen}
